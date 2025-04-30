32819740602e00e33f9fdfec5fe9fe2244b47919
yukii <lichtestv@gmail.com>
2025-04-30 17:22:34 +0200

    Start of new hyprland dotfiles

---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+print_logo() {
+  cat << "EOF"
+ 
+  ,---.   .--.   ____     __   ____    .-------.        _______   .---.  .---.         
+  |    \  |  |   \   \   /  /.'  __ `. |  _ _   \      /   __  \  |   |  |_ _|         
+  |  ,  \ |  |    \  _. /  '/   '  \  \| ( ' )  |     | ,_/  \__) |   |  ( ' )         
+  |  |\_ \|  |     _( )_ .' |___|  /  ||(_ o _) /   ,-./  )       |   '-(_{;}_)        
+  |  _( )_\  | ___(_ o _)'     _.-`   || (_,_).' __ \  '_ '`)     |      (_,_)         
+  | (_ o _)  ||   |(_,_)'   .'   _    ||  |\ \  |  | > (_)  )  __ | _ _--.   |         
+  |  (_,_)\  ||   `-'  /    |  _( )_  ||  | \ `'   /(  .  .-'_/  )|( ' ) |   |         
+  |  |    |  | \      /     \ (_ o _) /|  |  \    /  `-'`-'     / (_{;}_)|   | Arch Linux Configuration, Inspired by Typecraft        
+  '--'    '--'  `-..-'       '.(_,_).' ''-'   `'-'     `._____.'  '(_,_) '---' By: yukitty        
+                                                                                     
+EOF
+}
+
+clear 
+print_logo
+
+set -e
+
+if [ ! -f "packages.conf" ]; then
+  echo "Err! packages.conf not found! Exiting :c"
+  exit 1
+fi
+
+source packages.conf
+
+if [ ! -f "lib.sh" ]; then
+  echo "Err! libs.sh not found! Exiting :c"
+  exit 1
+fi
+
+source lib.sh 
+
+if [ ! -d "src" ]; then
+  echo "Err! Missing configs!"
+  exit 1
+fi
+
+echo "Upgrading to nyarch :3"
+sudo pacman -Syu --noconfirm
+
+if ! command -v yay &> /dev/null; then
+  echo "Installing yay"
+  sudo pacman -S --needed git base-devel --noconfirm
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  echo "building yay yippee"
+  makepkg -si --noconfirm
+  cd ..
+  rm -rf yay
+else
+  echo "yay is already installed, skipping that oopsy"
+fi
+
+install_packages "${desktop_env}"
+install_packages "${dev_tools}"
+install_packages "${apps}"
+install_packages "${fonts}"
+
+install_configs
---
+++ lib.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+is_installed() {
+  pacman -Qi "$1" &> /dev/null
+}
+
+is_group_installed() {
+  pacman -Qg "$1" &> /dev/null
+}
+
+install_packages() {
+  local packages=("$@")
+  local to_install=()
+
+  for pkg in "${packages[@]}"; do
+    if ! is_installed "$pkg" && ! is_group_installed "$pkg"; then
+      to_install+=("$pkg")
+    fi
+  done
+
+  if [ ${#to_install[@]} -ne 0 ]; then  
+    echo "Installing: ${to_install[*]}"
+    yay -S "${to_install[@]}"
+  fi
+} 
+
+install_nvchad() {
+  git clone https://github.com/NvChad/starter ~/.config/nvim
+}
+
+install_eww() {
+  cd 
+  git clone https://github.com/elkowar/eww
+  cd eww 
+  cargo build --release --no-default-features --features=wayland
+}
+
+make_symlinks() {
+  cd
+  ln -s $HOME/.cache/wal/colors.css $HOME/.config/dunst/colors.css
+  ln -s $HOME/.cache/wal/colors.scss $HOME/.config/eww/bar/colors.scss
+  ln -s $HOME/.cache/wal/colors.scss $HOME/.config/eww/menu/colors.scss
+  ln -s $HOME/.cache/wal/colors.scss $HOME/.config/eww/music/colors.scss
+}
+
+install_sddm() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+}
+
+install_configs() {
+  xdg-user-dir
+
+  cd src 
+  cp -r yukitty.png .config .wallpapers .zshenv $HOME/
+
+  sudo cp .config/rofi/launchers/type-6/launcher.sh /usr/local/bin/launch_rofi
+
+  wal -i $HOME/.wallpapers/three.png
+
+  install_nvchad
+  install_eww
+  install_sddm
+
+  make_symlinks
+}
